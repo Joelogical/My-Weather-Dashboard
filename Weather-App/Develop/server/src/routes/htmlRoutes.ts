0510841ca8d7190c5f54bdfd2002a5f42a52b9a7
Joelogical <jmsmith.coding@gmail.com>
2024-12-18 22:33:25 -0800

    debug weatherService.ts & weatherRoute.ts

--- Weather-App/Develop/server/src/routes/htmlRoutes.ts
+++ Weather-App/Develop/server/src/routes/htmlRoutes.ts
@@ -9,9 +9,9 @@ const __dirname = path.dirname(__filename);
 const router = Router();
 
 // Define route to serve index.html
-router.get('/', (req: Request, res: Response) => {
+router.get('/', (_: Request, res: Response) => {
   // Serve the index.html file from the dist folder
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../develop/client/index.html'));
 });
 
 export default router;
